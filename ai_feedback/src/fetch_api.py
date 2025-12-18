cca7df300b52e72467ca2cf0b1ee006aaf67cdd6
thorsanchez <ths281@hi.is>
2025-12-18 17:40:50 +0000

    comment feedback komið

--- ai_feedback/src/fetch_api.py
+++ ai_feedback/src/fetch_api.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-API_URL = "https://jsonplaceholder.typicode.com/comments"
+API_URL = "https://dummyjson.com/comments"
 
 def fetch_comments(limit: int = 20) -> pd.DataFrame:
     #fetch comment fra api og return pandas Dataframe
@@ -10,12 +10,17 @@ def fetch_comments(limit: int = 20) -> pd.DataFrame:
 
     data = response.json()
 
+    #extract nested body sem er i comments
+    comments_list = data.get("comments", [])
+
+
+
     #convert yfir í DataFrame
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(comments_list)
     #geyma fyrstu 20 bara 
     df = df.head(limit)
 
     #bara geyma þessa dalka
-    return df[['name', 'email', 'body']]
+    return df[['body']]
 
 #print(fetch_comments(2))
--- ai_feedback/src/main.py
+++ ai_feedback/src/main.py
@@ -4,3 +4,22 @@ from clean_text import clean_text
 from ai_summary import analyze_text
 
 output_path = "output/dailyreport.md"
+
+def main():
+    #fetch data fra api
+    df = fetch_comments(limit=5)
+
+    #clean comments
+    df["clean_body"] = df["body"].apply(clean_text)
+
+    #ai sum
+    report = analyze_text(df["clean_body"].tolist())
+
+    #vista i md skjal
+    with open(output_path, "w") as f:
+        f.write(str(report))
+    
+    print(f"report er í: {output_path}")
+
+if __name__ == "__main__":
+    main()
